3d2d7993449b85aee5a43d26d2bce2eeb82aae8e
kim <278362446@qq.com>
2020-05-30 23:46:29 +0800

    换到eclipse,由于idea的流程设计器不好用

--- activiti-process-engine/src/main/java/com/kim/activiti/process/engine/service/impl/ProcessInstancesServiceImpl.java
+++ activiti-process-engine/src/main/java/com/kim/activiti/process/engine/service/impl/ProcessInstancesServiceImpl.java
@@ -8,4 +8,9 @@ import com.kim.activiti.process.engine.service.ProcessInstancesService;
  * @date 2020/5/27
  */
 public class ProcessInstancesServiceImpl implements ProcessInstancesService {
+
+	@Override
+	public String startProcessInstanceByProcessDefId(String processDefinitionId) {
+		return null;
+	}
 }
